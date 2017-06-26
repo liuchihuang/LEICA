3a9e364a18b007224d74200053ffcc640f65a205
Chihuang <liuchihuang@gmail.com>
2017-06-26 15:06:22 -0400

    edit

--- data_handler.py
+++ data_handler.py
@@ -1,15 +1,10 @@
 import os
-
+from mlabwrap import mlab
 
 def get_hcp_resting(data_dir):
     subjects = os.listdir(data_dir)
     # HCP fix denoised resting state dataset
     runs = ['rfMRI_REST1_LR', 'rfMRI_REST1_RL', 'rfMRI_REST2_LR', 'rfMRI_REST2_RL']
-    run1 = 'MNINonLinear/Results/rfMRI_REST1_LR/rfMRI_REST1_LR_Atlas_MSMAll_hp2000_clean.dtseries.nii'
-    run2 = 'MNINonLinear/Results/rfMRI_REST1_RL/rfMRI_REST1_RL_Atlas_MSMAll_hp2000_clean.dtseries.nii'
-    run3 = 'MNINonLinear/Results/rfMRI_REST2_LR/rfMRI_REST2_LR_Atlas_MSMAll_hp2000_clean.dtseries.nii'
-    run4 = 'MNINonLinear/Results/rfMRI_REST2_RL/rfMRI_REST2_RL_Atlas_MSMAll_hp2000_clean.dtseries.nii'
-    run_list = [run1, run2, run3, run4]
     filenames = [os.path.join(data_dir, subject, 'MNINonLinear/Results/%s/%s_Atlas_MSMAll_hp2000_clean.dtseries.nii'
                               % (runs[i], runs[i])) for subject in subjects for i in range(4)]
     return filenames
